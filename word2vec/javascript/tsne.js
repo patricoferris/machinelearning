998473a50285a03c22a37d206338a02a353d712b
patricoferris <patrickferris17@gmail.com>
2018-08-09 08:51:06 +0100

    Create tsne.js

---
+++ word2vec/javascript/tsne.js
@@ -0,0 +1,7 @@
+/*
+*
+* This file contains an implementation of the t-Distributed Stochastic Neighbour Embedding algorithm
+* It is used in order to reduce the dimensionality of data whilst preserving the higher dimensional similarities that exist.
+* The algorithm can be found in this paper: http://jmlr.csail.mit.edu/papers/volume9/vandermaaten08a/vandermaaten08a.pdf
+*
+*/
